4bb21dd575b48b106ca4acb774bedb2d5795219e
Andrew <3922017@holycross.edu.ph>
2025-06-11 16:19:33 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+module.exports = {
+  darkMode: "class",
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
